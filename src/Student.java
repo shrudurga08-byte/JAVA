54aa9cb7852681a1c0a0a1215cb4010e9fda2e00
LENOVO <shrudurga08@gmail.com>
2026-08-11 19:52:08 +0530

    Day 1 of java

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+
+        // Method
+        void displayMessage() {
+            System.out.println("Hello! This is my Java method.");
+        }
+
+        // Main method
+        public static void main(String[] args) {
+
+            // Create object
+            Student student = new Student();
+
+            // Call the method
+            student.displayMessage();
+        }
+    }
