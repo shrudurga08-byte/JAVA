85eaf692f7ee4b6a2c512d58ea454f12f55a140d
LENOVO <shrudurga08@gmail.com>
2026-08-13 00:18:23 +0530

    LabA

---
+++ src/Lab_Practice/LabA.java
@@ -0,0 +1,18 @@
+package Lab_Practice;
+
+public class LabA {
+    public static void main(String[] args) {
+
+        int age = 18;
+        long population = 1400000000L;
+        double price = 99.99;
+        boolean isStudent = true;
+        char grade = 'A';
+
+        System.out.println("int: " + age);
+        System.out.println("long: " + population);
+        System.out.println("double: " + price);
+        System.out.println("boolean: " + isStudent);
+        System.out.println("char: " + grade);
+    }
+}
